e89e6fe7dfe7f0e3b6ceeafa0d362a81ebe71118
PhanThanhHuy22130105 <22130105@st.hcmuaf.edu.vn>
2025-12-20 20:44:01 +0700

    - add main navigation sidebar - Implement vertical layout with top (profile/menu) and bottom (settings) sections. - Styled with brand blue color and hover effects.
    Signed-off-by: PhanThanhHuy22130105 <22130105@st.hcmuaf.edu.vn>

--- src/components/Navigation/Sidebar.jsx
+++ src/components/Navigation/Sidebar.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import {MessageSquare, Contact, CheckSquare, Cloud, Settings, Monitor} from 'lucide-react';
+import './sidebar.css'
 
-function Sidebar() {
-  return <div>Sidebar</div>;
-}
+const Sidebar = () => {
+    return (<div className="sidebar-nav">
+            {/* Ảnh đại diện */}
+            <div className="user-profile-avatar">
+                <img
+                    src="https://img.tripi.vn/cdn-cgi/image/width=700,height=700/https://gcs.tripi.vn/public-tripi/tripi-feed/img/482752udT/anh-mo-ta.png"
+                    alt="profile"/>
+            </div>
+
+            {/* Thanh chức năng */}
+            <div className="nav-menu-items">
+                <div classname="nav-item active">
+                    <MessageSquare size={26}/>
+                </div>
+                <div className="nav-item">
+                    <Contact size={26}/>
+                </div>
+                <div className="nav-item">
+                    <CheckSquare size={26}/>
+                </div>
+            </div>
+
+            <div className="nav-group-bottom">
+                <div className="nav-item">
+                    <Cloud size={24}/>
+                </div>
+                <div className="nav-item">
+                    <Monitor size={24}/>
+                </div>
+                <div className="nav-item">
+                    <Settings size={24}/>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Sidebar;
